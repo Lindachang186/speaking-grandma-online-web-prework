41816a07f5f2f501128a8e1844fa9623ec8809de
Linda Chang <lindachang186@gmail.com>
2018-10-04 16:22:59 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.downcase
+  if phrase != phrase.upcase
     print "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     print "NO, NOT SINCE 1938!"
